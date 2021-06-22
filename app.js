8a403b9aea568bd048b94467d945b64121cf964f
= <wmurdoch@brownsteingroup.com>
2021-06-21 22:30:17 -0400

    env setup

--- app.js
+++ app.js
@@ -1,33 +1,40 @@
+//Import Node modules
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
+//Initialize app
 var app = express();
 
-// view engine setup
+//Set up routes
+var indexRouter = require('./routes/index');
+app.use('/', indexRouter);
+
+var usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
+//Read in custom environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+//View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
+//Tell app to use imported Node modules
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
+//Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
+//Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
