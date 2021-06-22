dec2c1147ff5935cbb104d72d64167e811d5c52b
Jon Gomez <jgomez@brownsteingroup.com>
2021-06-22 16:01:08 -0400

    Install firestore

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,35 @@
 var express = require('express');
 var router = express.Router();
 const jokes = require("give-me-a-joke");
+var db = firebase.firestore();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Map App' });
+  res.render('index', {
+    title: 'Map App'
+  });
+  db.collection("users").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+    })
+    .then((docRef) => {
+      console.log("Document written with ID: ", docRef.id);
+    })
+    .catch((error) => {
+      console.error("Error adding document: ", error);
+    });
 });
 
 jokes.getRandomDadJoke(function(joke) {
   console.log(joke);
+});
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: '### FIREBASE API KEY ###',
+  authDomain: '### FIREBASE AUTH DOMAIN ###',
+  projectId: '### CLOUD FIRESTORE PROJECT ID ###'
 });
 
 module.exports = router;
