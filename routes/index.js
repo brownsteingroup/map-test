9d219b8fd02cb75cb7078321d0f8c8e70781b959
Jon Gomez <jgomez@brownsteingroup.com>
2021-06-22 14:00:08 -0400

    Add jokes package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const jokes = require("give-me-a-joke");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Map App' });
+});
+
+jokes.getRandomDadJoke(function(joke) {
+  console.log(joke);
 });
 
 module.exports = router;
